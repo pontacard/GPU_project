1cc43e1bbde101d87329d3541fab609e2cbfebc6
pontacard <ryotatsumiryo288@gmail.com>
2024-05-07 19:07:38 +0900

    me:i

--- spin/macro_bihuraction.py
+++ spin/macro_bihuraction.py
@@ -31,7 +31,7 @@ def SOT_bihuraction(alpha, beta, gamma,ax,B,S0,omega, t,  t_eval,theta,Kx,Ky,Kz,
     for B0 in B0_eval:
         duf = Sin_SOT(alpha, gamma, B, S0, t, t_eval, [0, B0, 0], omega, theta, Kx, Ky, Kz, beta, start, stop)
         duf.history()
-        poi = duf.poincore(ax,4995000, 5000001)
+        poi = duf.poincore(ax,7880000,8000001)
         B0_list = [B0] * len(poi)
         poi_list = np.append(poi_list, poi)
         B_list = np.append(B_list, B0_list)
@@ -39,7 +39,7 @@ def SOT_bihuraction(alpha, beta, gamma,ax,B,S0,omega, t,  t_eval,theta,Kx,Ky,Kz,
     #plt.ylim(-0.2, 1)
     plt.gca().set_aspect(4)
     plt.scatter(B_list, poi_list, c='b', s=1)
-    plt.savefig(f"SOT_Sx_bihuraction_{B[0]}_{Ky}_{omega[1]}GHz.pdf")
+    plt.savefig(f"SOT_Sx_bihuraction_[{B[0]}_{B[1]}_{B[2]}]_[{Kx}_{Ky}_{Kz}]_{omega[1]}_komine.pdf")
 
 
 def FMR_thermal_bihuraction(alpha, beta, gamma,ax,B,S0,omega, t,  t_eval,theta,Kx,Ky,Kz,start,stop,sigma,ther_dt,sta_B0,end_B0,step_B0):
@@ -92,5 +92,5 @@ Bx = 165
 omega = 20.232
 
 #FMR_thermal_bihuraction(0.05, 0,0.17,1,[Bx,0,0],S0,[0,omega,0],t,t_eval,[0,0,0],0 , 200,0,0,9000,sigma_Bthe,ther_dt,4,25,301)
-FMR_bihuraction(0.05, 0,0.176335977,1,[Bx,0,0],S0,[0,omega,0],t,t_eval,[0,0,0],0,200,0,0,9000,4,20,161)
-#SOT_bihuraction(0.05, 0,0.17,1,[Bx,0,0],S0,[omega,omega,0],t,t_eval,[0,0,0],0 , 200,0,0,9000,4,25,601)
+#FMR_bihuraction(0.05, 0,0.176335977,1,[Bx,0,0],S0,[0,omega,0],t,t_eval,[0,0,0],0,200,0,0,9000,4,20,161)
+SOT_bihuraction(0.05, 0,0.176335977,1,[Bx,0,0],S0,[0,omega,0],t,t_eval,[0,0,0],0 , 200,0,0,9000,4,24,321)
