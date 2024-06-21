7f639bb00ebefc91437c8249828f4be5f109ccb9
pontacard <ryotatsumiryo288@gmail.com>
2024-06-21 22:47:44 +0900

    jo yuri

--- spin/Lyapunov_map.py
+++ spin/Lyapunov_map.py
@@ -96,10 +96,10 @@ def thermal_FMR_Lyapunov_map(alpha, gamma,B,K,ax,omega,phase,sigma_Bthe, ther_dt
 t = [0,800]
 t_eval = np.linspace(*t, 8000001)
 #Lyapunov_map(1,32,176,8.092,t,t_eval,[0.4264,0,0], 2, 10, 400)
-#FMR_Lyapunov_map(0.05,0.176335977,165,200,20.232,t,t_eval,[np.pi/2,0.6005,0],4,24,321,[0.01,0,0], 10001,7000000,aspect = 2)
-SOT_Lyapunov_map(0.05,0.176335977,165,200,20.232,t,t_eval,[np.pi/2,0.6435,0],4,24,321,[0.01,0,0], 10001,7000000,aspect = 2)
+FMR_Lyapunov_map(0.05,0.176335977,0,200,20.232,t,t_eval,[np.pi/2,0.6005,0],4,250,501,[0.01,0,0], 10001,7000000,aspect = 2)
+#SOT_Lyapunov_map(0.05,0.176335977,165,200,20.232,t,t_eval,[np.pi/2,0.6435,0],0,250,501,[0.01,0,0], 10001,7000000,aspect = 2)
 
-B = [165,0,0]
+B = [0,0,0]
 K = [0,200,0]
 phase = [0,0,0]
 #ax_FMR_Lyapunov_map(0.05,0.17,B,K,1,51.019,phase,t,t_eval,[np.pi/2,0.6435,0],4,40,2)
