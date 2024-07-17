122d9591ff6ede750a20bca0285d2ae7474f9d0b
pontacard <ryotatsumiryo288@gmail.com>
2024-07-17 12:03:35 +0900

    girls of the year

--- spin/macro_bihuraction.py
+++ spin/macro_bihuraction.py
@@ -14,7 +14,7 @@ def FMR_bihuraction(alpha, beta, gamma,ax,B,S0,omega, t,  t_eval,theta,Kx,Ky,Kz,
     for B0 in B0_eval:
         duf = FMR_spin(alpha,gamma,B,S0,t,t_eval,[0,B0,0],omega,theta,Kx,Ky,Kz,beta,start,stop)
         duf.history()
-        poi = duf.poincore(ax,399000000,400000001)
+        poi = duf.poincore(ax,39900,40001)
         B0_list = [B0] * len(poi)
         poi_list = np.append(poi_list, poi)
         B_list = np.append(B_list,B0_list)
@@ -97,8 +97,8 @@ print(sigma_Bthe)
 
 S0 = [1, 0, 0]
 
-t = [0, 4000]  # t(時間)が0〜100まで動き、その時のfを求める。
-t_eval = np.linspace(*t, 400000001)
+t = [0, 4]  # t(時間)が0〜100まで動き、その時のfを求める。
+t_eval = np.linspace(*t, 40001)
 mu_0 = 1.2
 B0 = 10.5
 Bx = 160
@@ -106,5 +106,5 @@ Bz = 0
 omega = 28.4
 
 #FMR_thermal_bihuraction(0.05, 0,0.17,1,[Bx,0,0],S0,[0,omega,0],t,t_eval,[0,0,0],0 , 200,0,0,9000,sigma_Bthe,ther_dt,4,25,301)
-FMR_bihuraction(0.02, 0,0.176335977,1,[Bx,0,Bz],S0,[0,omega,0],t,t_eval,[0,0,0],0,0,0,0,900000,0,25,251)
+FMR_bihuraction(0.02, 0,0.176335977,1,[Bx,0,Bz],S0,[0,omega,0],t,t_eval,[0,0,0],0,0,0,0,900000,0,25,2)
 #SOT_bihuraction(0.05, 0,0.176335977,1,[Bx,0,0],S0,[0,omega,0],t,t_eval,[0,0,0],0 , 200,0,0,9000,4,25,211)
