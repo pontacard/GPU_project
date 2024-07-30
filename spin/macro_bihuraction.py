688c18a5865d4d49035aec13ccc889ea58db3dc6
pontacard <ryotatsumiryo288@gmail.com>
2024-07-30 17:20:19 +0900

    girls of the year

--- spin/macro_bihuraction.py
+++ spin/macro_bihuraction.py
@@ -107,5 +107,5 @@ omega = 21.36
 
 
 #FMR_thermal_bihuraction(0.05, 0,0.17,1,[Bx,0,0],S0,[0,omega,0],t,t_eval,[0,0,0],0 , 200,0,0,9000,sigma_Bthe,ther_dt,4,25,301)
-FMR_bihuraction(0.02, 0,0.176335977,1,[Bx,0,Bz],S0,[0,omega,0],t,t_eval,[0,0,0],0,200,0,0,900000,0,25,251)
+FMR_bihuraction(0.02, 0,0.176335977,1,[Bx,0,Bz],S0,[0,omega,0],t,t_eval,[0,0,0],0,200,0,0,900000,10,11,21)
 #SOT_bihuraction(0.05, 0,0.176335977,1,[Bx,0,0],S0,[0,omega,0],t,t_eval,[0,0,0],0 , 200,0,0,9000,4,25,211)
